c2422aeb8a3eaa33157d2e1da2cef59aab5f35af
Animesh <animeshiit0806@gmail.com>
2025-10-15 01:41:26 +0530

    Initial commit: CodeAstra frontend and backend setup

---
+++ backend/src/test/java/com/codeastra/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeastra.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
